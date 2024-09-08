104cf374ac0601cf8a3de2786c90438149e25aba
Tran Minh Thanh <trmthanh220895@gmail.com>
2024-09-09 01:46:33 +0800

    PPP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,32 @@ export default function RootLayout({
         <head>
           {/* Standard metadata */}
           <meta name="facebook-domain-verification" content="w484tu2k8ly6b5p1yl55mtjtwmbbqn" />
-          {/* Additional meta tags */}
+
+          {/* Facebook Pixel Script */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s) {
+                  if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '980884502489134');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=980884502489134&ev=PageView&noscript=1"
+            />
+          </noscript>
         </head>
         <body className={dancingScript.className}>
           {children}
