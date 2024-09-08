18d4176cdf1532445f8dfba6475f552309f8fe58
Tran Minh Thanh <trmthanh220895@gmail.com>
2024-09-08 23:54:51 +0800

    pppp

--- src/app/components/SearchProduct/SearchProdct.tsx
+++ src/app/components/SearchProduct/SearchProdct.tsx
@@ -8,7 +8,7 @@ const SearchProdct = () => {
         <h1 className="text-[1.5rem] text-blue-600 md:text-[2rem] text-center mb-4">
           WHAT ARE YOU LOOKING FOR, MY FRIENDS?
         </h1>
-        <div className="flex items-center w-[15rem] md:w-[22rem] bg-white rounded-md px-2 py-1">
+        <div className="flex items-center w-[15rem] lg:w-[22rem] bg-white rounded-md px-2 py-1">
           <CiSearch size={24} className="text-gray-700 hover:font-bold hover:text-white hover:scale-110 cursor-pointer" />
           <input
             type="text"
