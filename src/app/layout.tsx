f7cd9cc290a4ee4b43f40a19eb0b221bd01f0e5d
Tran Minh Thanh <trmthanh220895@gmail.com>
2024-09-09 02:04:43 +0800

    pppsssssss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <StoreProvider>
+
       <html lang="en">
         <head>
           {/* Standard metadata */}
@@ -61,61 +61,13 @@ export default function RootLayout({
             />
           </noscript>
         </head>
+        <StoreProvider>
         <body className={dancingScript.className}>
           {children}
         </body>
+        </StoreProvider>
       </html>
-    </StoreProvider>
+
   );
 }
 
-
-
-// import type { Metadata } from "next";
-// import { Inter,Dancing_Script } from "next/font/google";
-// import "./globals.css";
-// import StoreProvider from "./storeProvider/StoreProvider";
-// // const inter = Inter({ subsets: ["latin"] });
-// import Head from 'next/head';
-
-// const dancingScript = Dancing_Script({
-//   subsets: ['latin'],
-//   weight: ['400', '500', '600', '700']
-// });
-
-// export const metadata: Metadata = {
-//   title: "Selcool",
-//   description: "Welcome to Selcool's Home",
-//   icons:{
-//     icon:['/favicon.ico'],
-//     apple:['/apple-touch-icon.png'],
-//     shortcut:['/apple-touch-icon.png']
-//   }
-// };
-
-
-
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <StoreProvider>
-
-//     <html lang="en">
-  
-//     <head>
-//         <Head>
-//           <meta name="facebook-domain-verification" content="w484tu2k8ly6b5p1yl55mtjtwmbbqn" />
-//         </Head>
-//       </head>
-//       <body className={dancingScript.className}>
-      
-//         {children}
-//       </body>
-//     </html>
-//     </StoreProvider>
-//   );
-// }
